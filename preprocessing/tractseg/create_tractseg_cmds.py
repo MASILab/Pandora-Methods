4fd2c72bc345b027c1579142b4b821c05d325bd7
Colin B Hansen <hansencb@hickory.accre.vanderbilt.edu>
2021-07-23 11:43:38 -0500

    fixing singularity commands

--- preprocessing/tractseg/create_tractseg_cmds.py
+++ preprocessing/tractseg/create_tractseg_cmds.py
@@ -1,12 +1,13 @@
 import os
 
-projs = ['/nfs/masi/hansencb/t1_tract_data/raw_data/NORMAL',
-         '/nfs/masi/hansencb/t1_tract_data/raw_data/HCP',
-         '/nfs/masi/hansencb/t1_tract_data/raw_data/BLSA']
+#projs = ['/nfs/masi/hansencb/t1_tract_data/raw_data/NORMAL',
+#         '/nfs/masi/hansencb/t1_tract_data/raw_data/HCP',
+#         '/nfs/masi/hansencb/t1_tract_data/raw_data/BLSA']
 
+projs = ['/nfs/masi/hansencb/t1_tract_data/raw_data/HCP']
 
 work_dir = '/scratch/hansencb/tract_preprocessing/tractseg'
-cmd = 'bash /scratch/hansencb/tract_preprocessing/tractseg/run_tractseg.sh {} {}'
+cmd = 'bash /scratch/hansencb/tract_preprocessing/tractseg/run_tractseg.sh {} {}\n'
       
 out_file = 'tractseg_cmds.txt'
 
@@ -22,8 +23,6 @@ with open(out_file, 'w') as f:
                         work_sess_dir = os.path.join(work_dir, subj, sess)
                         dwi_path = os.path.join(sess_dir, 'dwi')
                         out_path = os.path.join(sess_dir, 'derivatives', 'tractseg')
-                        if os.path.isfile(dwi_path):# and not os.path.isdir(os.path.join(sess_dir, 'derivatives', 'tractseg')):
-                            f.write(cmd.format(dwi_path, out_path)
-
-
-
+                        if os.path.isdir(dwi_path):
+                            # and not os.path.isdir(os.path.join(sess_dir, 'derivatives', 'tractseg')):
+                            f.write(cmd.format(dwi_path, out_path))
